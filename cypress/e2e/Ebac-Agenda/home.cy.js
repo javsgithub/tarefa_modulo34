2201d4f37e1c3af5e418cdc83ed9208be78c66ea
Jefferson Santos <doctorjeffbr@yahoo.com.br>
2024-09-24 16:42:45 -0300

    Ajustando o teste de alteração de contatos

--- cypress/e2e/Ebac-Agenda/home.cy.js
+++ cypress/e2e/Ebac-Agenda/home.cy.js
@@ -13,20 +13,17 @@ describe("Testes para a página/agenda de contatos EBAC", () => {
         cy.get("#root").should("contain.text", "Jefferson Santos")
     })
 
-    it("Deve preparar os dados para edição", () => {
+    it("Deve alterar o novo contato e salvar a(s) alteração(ões)", () => {
         cy.get(".edit").last().click()
         cy.get("input[type='text']").should("contain.value", "Jefferson Santos")
         cy.get("input[type='email']").should("contain.value", "jeffersonsantos@gmail.com")
         cy.get("input[type='tel']").should("contain.value", "71 999999999")
-    })
-    
-    it("Deve alterar o novo contato e salvar a(s) alteração(ões)", () => {
         cy.get("input[type='tel']").clear()  
         cy.get("input[type='tel']").type("27 99199 70, se falhar, 70 de novo")  
         cy.get(".alterar").click()
         cy.get("#root").should("contain.text", "27 99199 70, se falhar, 70 de novo")        
     })
-
+    
     it("Deve remover o contato adicionado da lista", () => {
         cy.get(".delete").last().click()
         cy.get("#root").should("not.contain.text", "Jefferson Santos")
